cb591343fa6dafc5badd2163d28acdf6bb410640
Julio Araujo <julio.araujo@gmail.com>
2022-09-27 15:39:46 -0300

    configs

---
+++ mobile/src/components/BackGround/index.tsx
@@ -0,0 +1,7 @@
+import { ImageBackground } from "react-native";
+
+import { styles } from "./styles";
+
+export function BackGround() {
+  return <ImageBackground style={styles.container}></ImageBackground>;
+}
---
+++ mobile/src/utils/games.ts
@@ -0,0 +1,8 @@
+export const games = [
+  {
+    id: "1",
+    name: "League of Legends",
+    ads: "4",
+    cover: require("../assets/games/g1.png"),
+  },
+];
