22f52bfd0fffab6f8ec2e5b70243991ec6672be0
Julio Araujo <julio.araujo@gmail.com>
2022-10-06 10:05:19 -0300

    creating Database

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,13 +10,21 @@ app.post("/ads", (req, res) => {
   return res.status(201).json("createAdd");
 });
 
-app.get("/ads", (request, response) => {
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
+
   response.json([
     { id: 1, name: "Advertising" },
     { id: 2, name: "Advertising" },
     { id: 3, name: "Advertising" },
     { id: 4, name: "Advertising" },
   ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const adId = request.params.id;
+
+  response.json([]);
 });
 
 app.listen(3333);
