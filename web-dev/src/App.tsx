2db4a6f87abd3ce9c580f9ad34135e27e83969d6
Julio Araujo <julio.araujo@gmail.com>
2022-10-31 10:14:41 -0300

    chore: fetching data

--- web-dev/src/App.tsx
+++ web-dev/src/App.tsx
@@ -4,19 +4,44 @@ import logoImg from "/Logo.svg";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdd } from "./components/CreateAdd";
 import { LandingHeader } from "./components/LandingHeader";
+import { useState, useEffect } from "react";
+
+interface Game {
+  id: string;
+  title: string;
+  bannerURL: string;
+  _count: {
+    ads: number;
+  };
+}
 
 function App() {
+  // <> é chamado de generic (como um parâmetro de tipagem (ts only))
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []); //array de dependências. Caso seja passado vazio, o estado só será atualizado uma vez!
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="Logotipo" />
       <LandingHeader />
+
       <div className="grid grid-cols-6 gap-6 mt-8">
-        <GameBanner bannerURL="/g1.png" title="League of Legends" adCount={4} />
-        <GameBanner bannerURL="/g2.png" title="League of Legends" adCount={4} />
-        <GameBanner bannerURL="/g3.png" title="League of Legends" adCount={4} />
-        <GameBanner bannerURL="/g4.png" title="League of Legends" adCount={4} />
-        <GameBanner bannerURL="/g5.png" title="League of Legends" adCount={4} />
-        <GameBanner bannerURL="/g6.png" title="League of Legends" adCount={4} />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              bannerURL={game.bannerURL}
+              title={game.title}
+              adCount={game._count.ads}
+            />
+          );
+        })}
       </div>
       <CreateAdd />
     </div>
